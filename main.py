58d9cc643aeeb8d358b8ea63004cbad62ad491a8
Vikhyathraj Shetty K <itsvikhyathraj@gmail.com>
2023-07-26 18:15:03 +0530

    merged file_types and paths to one dict

--- main.py
+++ main.py
@@ -3,42 +3,61 @@ import shutil
 
 cwd = "/Users/n0tv1cky/Downloads"
 
-file_types = {
-    'Media': ['.jpg', '.jpeg', '.png', 'mpeg', 'mp4', '.mov'],
-    'Docs': ['.pdf', '.docx', '.pptx'],
-    'DMG files': ['.dmg'],
-    'App installations': ['.app'],
-    'Compressed files': ['.zip', '.7z', '.rar'],
-    'Misc': []
+file_data = {
+    'Media': {
+        'extensions': ['.jpg', '.jpeg', '.png', 'mpeg', '.mp4', '.mov'],
+        'path': os.path.join(cwd, "Media")
+    },
+    'Docs': {
+        'extensions': ['.pdf', '.docx', '.pptx'],
+        'path': os.path.join(cwd, "Docs")
+    },
+    'DMG files': {
+        'extensions': ['.dmg'],
+        'path': os.path.join(cwd, "DMG files")
+    },
+    'App installations': {
+        'extensions': ['.app'],
+        'path': os.path.join(cwd, "App installations")
+    },
+    'Compressed files': {
+        'extensions': ['.zip', '.7z', '.rar'],
+        'path': os.path.join(cwd, "Compressed files")
+    },
+    'Misc': {
+        'extensions': [],  # An empty list since there are no specific extensions for Misc
+        'path': os.path.join(cwd, "Misc")
+    }
 }
 
-paths = {
-    'Media': os.path.join(cwd, "Media"),
-    'Docs': os.path.join(cwd, "Docs"),
-    'DMG files': os.path.join(cwd, "DMG files"),
-    'App installations': os.path.join(cwd, "App installations"),
-    'Compressed files': os.path.join(cwd, "Compressed files"),
-    'Misc': os.path.join(cwd, "Misc")
-}
+excluded_files = ['.DS_Store', '.localized']
 
-excluded_files = {'.DS_Store', '.localized'} 
-
-for folder in file_types:
-    if not os.path.isdir(paths[folder]):
-        os.makedirs(paths[folder])
+for folder, data in file_data.items():
+    if not os.path.isdir(data['path']):
+        os.makedirs(data['path'])
         print(f"{folder} folder created")
 
-files = [f for f in os.listdir(cwd) if os.path.isfile(os.path.join(cwd, f))]
+files = [f for f in os.listdir(cwd) if os.path.isfile(
+    os.path.join(cwd, f)) or f.endswith('.app') and f not in excluded_files]
 
 if files:
     for file in files:
         if file not in excluded_files:
             file_path = os.path.join(cwd, file)
+            moved = False
 
-            for file_type, extensions in file_types.items():
+            for file_type, file_info in file_data.items():
+                extensions = file_info['extensions']
+                destination_path = file_info['path']
+
                 if file_path.endswith(tuple(extensions)):
                     print(f"Moved {file} to {file_type} folder")
-                    shutil.move(file_path, paths[file_type])
-                else:
-                    print(f"Moved {file} to 'Misc' folder")
-                    shutil.move(file_path, paths['Misc'])
+                    shutil.move(file_path, destination_path)
+                    moved = True
+                    break
+            if not moved:
+                print(f"Moved {file} to 'Misc' folder")
+                shutil.move(file_path, file_data['Misc']['path'])
+
+    print("Sorting operation completed succesfully")
+print("No files to sort")
